a5d35e4a57eaab4f78750cd7889a31c10386923f
akash yanakandla <akashyanakandla0073@gmail.com>
2024-06-07 03:36:39 +0530

    this is the first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_trial3/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
